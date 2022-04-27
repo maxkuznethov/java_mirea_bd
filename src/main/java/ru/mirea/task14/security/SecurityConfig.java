2af90a3762183a2462cda96128b4869c3114dd04
Maxim Kuznethov <max.kuznethov03@gmail.com>
2022-04-27 17:58:54 +0300

    task24

--- src/main/java/ru/mirea/task14/security/SecurityConfig.java
+++ src/main/java/ru/mirea/task14/security/SecurityConfig.java
@@ -1,4 +1,39 @@
 package ru.mirea.task14.security;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Max")
+                .password(encoder().encode("12345"))
+                .roles("admin_role");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws
+            Exception {
+        http.csrf().disable().cors().disable()
+                .authorizeRequests().antMatchers("/login",
+                        "/logout").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
+
+    @Bean
+    PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
